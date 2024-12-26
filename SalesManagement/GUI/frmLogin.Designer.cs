7800b6d9662e28408d8fe8f26f985a6eec196113
cuthanhcam <cuthanhcam04@gmail.com>
2024-12-26 11:23:51 +0700

    Add frmLogin

--- SalesManagement/GUI/frmLogin.Designer.cs
+++ SalesManagement/GUI/frmLogin.Designer.cs
@@ -28,13 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlUsername = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlPassword = new System.Windows.Forms.Panel();
+            this.picPassword = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlUsername.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.pnlPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(141, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(134, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGIN";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(41, 6);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(250, 27);
+            this.txtUsername.TabIndex = 2;
+            this.txtUsername.Enter += new System.EventHandler(this.txtUsername_Enter);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUsername_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(41, 6);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(250, 27);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(66, 411);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(250, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Location = new System.Drawing.Point(66, 466);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(250, 40);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // pnlUsername
+            // 
+            this.pnlUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlUsername.Controls.Add(this.pictureBox2);
+            this.pnlUsername.Controls.Add(this.txtUsername);
+            this.pnlUsername.Location = new System.Drawing.Point(37, 254);
+            this.pnlUsername.Name = "pnlUsername";
+            this.pnlUsername.Size = new System.Drawing.Size(300, 40);
+            this.pnlUsername.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pnlPassword
+            // 
+            this.pnlPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPassword.Controls.Add(this.picPassword);
+            this.pnlPassword.Controls.Add(this.txtPassword);
+            this.pnlPassword.Location = new System.Drawing.Point(37, 310);
+            this.pnlPassword.Name = "pnlPassword";
+            this.pnlPassword.Size = new System.Drawing.Size(300, 40);
+            this.pnlPassword.TabIndex = 7;
+            // 
+            // picPassword
+            // 
+            this.picPassword.Image = ((System.Drawing.Image)(resources.GetObject("picPassword.Image")));
+            this.picPassword.Location = new System.Drawing.Point(4, 4);
+            this.picPassword.Name = "picPassword";
+            this.picPassword.Size = new System.Drawing.Size(30, 30);
+            this.picPassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPassword.TabIndex = 4;
+            this.picPassword.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.pnlPassword);
+            this.Controls.Add(this.pnlUsername);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlUsername.ResumeLayout(false);
+            this.pnlUsername.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.pnlPassword.ResumeLayout(false);
+            this.pnlPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel pnlUsername;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel pnlPassword;
+        private System.Windows.Forms.PictureBox picPassword;
     }
 }
 
--- SalesManagement/GUI/frmLogin.cs
+++ SalesManagement/GUI/frmLogin.cs
@@ -16,5 +16,108 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //SetFormStyle(this);  // Áp dụng UI cho toàn bộ form
+                //CustomizeDesign();   // Tùy chỉnh thiết kế các control
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // Thiết lập UI Form
+        private void SetFormStyle(Form form)
+        {
+            form.FormBorderStyle = FormBorderStyle.FixedSingle;
+            form.BackColor = Color.White;
+            form.Font = new Font("Segoe UI", 10);
+            form.ForeColor = Color.Black;
+            form.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        // Thiết kế Control như Panel, Button
+        private void CustomizeDesign()
+        {
+            // Panel chính (Fill toàn bộ form)
+            Panel panelMain = new Panel();
+            panelMain.Dock = DockStyle.Fill;
+            panelMain.BackColor = Color.White;
+            this.Controls.Add(panelMain);
+
+            // Panel bên trái (Sidebar)
+            Panel panelLeft = new Panel();
+            panelLeft.Dock = DockStyle.Left;
+            panelLeft.Width = 300;
+            panelLeft.BackColor = Color.FromArgb(41, 128, 185);  // Màu xanh
+            this.Controls.Add(panelLeft);
+
+            // Label chào mừng
+            Label lblWelcome = new Label();
+            lblWelcome.Text = "Welcome to Service Management";
+            lblWelcome.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            lblWelcome.ForeColor = Color.White;
+            lblWelcome.TextAlign = ContentAlignment.MiddleCenter;
+            lblWelcome.Dock = DockStyle.Top;
+            panelLeft.Controls.Add(lblWelcome);
+
+            // Nút Login
+            Button btnLogin = new Button();
+            btnLogin.Text = "LOGIN";
+            btnLogin.Size = new Size(250, 40);
+            btnLogin.Location = new Point(180, 220);
+            btnLogin.BackColor = Color.FromArgb(41, 128, 185);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.Click += btnLogin_Click;
+            panelMain.Controls.Add(btnLogin);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Login Clicked!");
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                txtUsername.Text = "Username";
+                txtUsername.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.PasswordChar = '•';
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Gray;
+                txtPassword.PasswordChar = '\0';
+            }
+        }
     }
 }
