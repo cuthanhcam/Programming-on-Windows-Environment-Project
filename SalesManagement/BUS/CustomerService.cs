a30cf6fada6cf7af159c9a6f88a8e287f013f9dc
cuthanhcam <cuthanhcam04@gmail.com>
2024-12-30 22:56:14 +0700

    Update BUS.Service..

---
+++ SalesManagement/BUS/CustomerService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class CustomerService
+    {
+        public static List<DAL.Entities.Customer> GetAll()
+        {
+            return BUS.CustomerService.GetAll();
+        }
+
+        public static DAL.Entities.Customer GetCustomerById(int id)
+        {
+            return BUS.CustomerService.GetCustomerById(id);
+        }
+
+        public static bool AddCustomer(DAL.Entities.Customer customer)
+        {
+            return BUS.CustomerService.AddCustomer(customer);
+        }
+
+        public static bool UpdateCustomer(DAL.Entities.Customer customer)
+        {
+            return BUS.CustomerService.UpdateCustomer(customer);
+        }
+
+        public static bool DeleteCustomer(int id)
+        {
+            return BUS.CustomerService.DeleteCustomer(id);
+        }
+    }
+}
