0c399739c0ad1e82d8de00db7a1faa4aa36d4229
cuthanhcam <cuthanhcam04@gmail.com>
2025-01-05 18:35:55 +0700

    Update Form Components

---
+++ SalesManagement/GUI/Form Components/frmPurchaseHistory.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GUI.Form_Components
+{
+    partial class frmPurchaseHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Processor", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Motherboard", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("CPU Cooler", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("Case", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup5 = new System.Windows.Forms.ListViewGroup("Graphic Card", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup6 = new System.Windows.Forms.ListViewGroup("RAM", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup7 = new System.Windows.Forms.ListViewGroup("Storage", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup8 = new System.Windows.Forms.ListViewGroup("Case Cooler", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup9 = new System.Windows.Forms.ListViewGroup("Power Supply", System.Windows.Forms.HorizontalAlignment.Left);
+            this.lstPurchaseHistory = new System.Windows.Forms.ListView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstPurchaseHistory
+            // 
+            this.lstPurchaseHistory.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lstPurchaseHistory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstPurchaseHistory.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPurchaseHistory.FullRowSelect = true;
+            listViewGroup1.Header = "Processor";
+            listViewGroup1.Name = "listViewGroupProcessor";
+            listViewGroup2.Header = "Motherboard";
+            listViewGroup2.Name = "listViewGroupMotherboard";
+            listViewGroup3.Header = "CPU Cooler";
+            listViewGroup3.Name = "listViewGroupCPUCooler";
+            listViewGroup4.Header = "Case";
+            listViewGroup4.Name = "listViewGroupCase";
+            listViewGroup5.Header = "Graphic Card";
+            listViewGroup5.Name = "listViewGroupGraphicCard";
+            listViewGroup6.Header = "RAM";
+            listViewGroup6.Name = "listViewGroupRAM";
+            listViewGroup7.Header = "Storage";
+            listViewGroup7.Name = "listViewGroupStorage";
+            listViewGroup8.Header = "Case Cooler";
+            listViewGroup8.Name = "listViewGroupCaseCooler";
+            listViewGroup9.Header = "Power Supply";
+            listViewGroup9.Name = "listViewGroupPowerSupply";
+            this.lstPurchaseHistory.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2,
+            listViewGroup3,
+            listViewGroup4,
+            listViewGroup5,
+            listViewGroup6,
+            listViewGroup7,
+            listViewGroup8,
+            listViewGroup9});
+            this.lstPurchaseHistory.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstPurchaseHistory.HideSelection = false;
+            this.lstPurchaseHistory.Location = new System.Drawing.Point(12, 12);
+            this.lstPurchaseHistory.MultiSelect = false;
+            this.lstPurchaseHistory.Name = "lstPurchaseHistory";
+            this.lstPurchaseHistory.Size = new System.Drawing.Size(470, 501);
+            this.lstPurchaseHistory.TabIndex = 37;
+            this.lstPurchaseHistory.TileSize = new System.Drawing.Size(500, 30);
+            this.lstPurchaseHistory.UseCompatibleStateImageBehavior = false;
+            this.lstPurchaseHistory.View = System.Windows.Forms.View.Details;
+            this.lstPurchaseHistory.DoubleClick += new System.EventHandler(this.lstPurchaseHistory_DoubleClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Bahnschrift", 13F);
+            this.btnClose.Location = new System.Drawing.Point(382, 530);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 30);
+            this.btnClose.TabIndex = 38;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmPurchaseHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(494, 572);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lstPurchaseHistory);
+            this.Name = "frmPurchaseHistory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CustomerHistory";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPurchaseHistory_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstPurchaseHistory;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ SalesManagement/GUI/Form Components/frmPurchaseHistory.cs
@@ -0,0 +1,88 @@
+﻿using BUS;
+using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.Form_Components
+{
+    public partial class frmPurchaseHistory : Form
+    {
+        private readonly List<Order> _purchaseHistory;
+        private readonly OrderService _orderService;
+
+        public frmPurchaseHistory(List<Order> purchaseHistory, OrderService orderService)
+        {
+            InitializeComponent();
+            _purchaseHistory = purchaseHistory;
+            _orderService = orderService;
+            InitializeListViewColumns();
+            LoadPurchaseHistory();
+            
+        }
+
+        private void InitializeListViewColumns()
+        {
+            lstPurchaseHistory.Columns.Clear();
+            lstPurchaseHistory.Columns.Add("OrderID", 100);
+            lstPurchaseHistory.Columns.Add("OrderDate", 150);
+            lstPurchaseHistory.Columns.Add("TotalAmount", 120);
+            lstPurchaseHistory.Columns.Add("Status", 100);
+
+            lstPurchaseHistory.View = View.Details;
+        }
+
+        private void LoadPurchaseHistory()
+        {
+            lstPurchaseHistory.Items.Clear();
+            lstPurchaseHistory.Groups.Clear();
+            foreach (var order in _purchaseHistory)
+            {
+                var item = new ListViewItem(order.OrderID.ToString());
+                item.SubItems.Add(order.OrderDate.ToString("dd/MM/yyyy"));
+                item.SubItems.Add(order.TotalAmount.ToString("C"));
+                item.SubItems.Add(order.Status);
+
+                lstPurchaseHistory.Items.Add(item);
+            }
+        }
+
+        private void frmPurchaseHistory_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //var result = MessageBox.Show("Are you sure you want to close this window?", "Confirm Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            //if (result == DialogResult.No)
+            //{
+            //    e.Cancel = true; // Hủy sự kiện đóng form
+            //}
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lstPurchaseHistory_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstPurchaseHistory.SelectedItems.Count > 0)
+            {
+                int orderID = int.Parse(lstPurchaseHistory.SelectedItems[0].Text);
+
+                try
+                {
+                    var orderDetailForm = new frmOrderDetail(orderID, _orderService);
+                    orderDetailForm.ShowDialog();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading order details: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
